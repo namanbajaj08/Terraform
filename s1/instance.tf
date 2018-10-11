1dc28d1c7ca32eef1cfd2891dd9bc84853cc3eab
Naman <namanbajaj08@gmail.com>
2018-10-11 19:08:11 +0530

    script 1 creating ec2 instance

---
+++ s1/instance.tf
@@ -0,0 +1,10 @@
+provider "aws"{
+  access_key = "access_key"
+  secret_key = "secret_key"
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "example"{
+  ami = "ami-0d773a3b7bb2bb1c1"
+  instance_type = "t2.micro"
+}
